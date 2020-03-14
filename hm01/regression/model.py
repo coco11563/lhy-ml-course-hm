0dd5f577cee7b445e587d5a81db5f2224d6ee6b3
coco11563@yeah.net <coco11563@yeah.net>
2020-03-14 20:44:25 +0800

    Initial commit

---
+++ hm01/regression/model.py
@@ -0,0 +1,11 @@
+import numpy as np
+class RegressionModel() :
+    def __init__(self, data, lr):
+        self.data = data
+
+
+    def fit(self):
+        return None
+
+    def predict(self, value):
+        return None
